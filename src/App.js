fcefe50ed9c8f69317fa05de76bcea37a754d4e7
James Blackmon <jkyle.blackmon@gmail.com>
2024-03-05 23:26:37 -0500

    UI up and running. React is working well

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route exact path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/projects' element={<Projects/>} />
